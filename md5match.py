f2839b80f7f587796e62b9302a77aa89bbad6b30
Jiawen Chen <jiawen@gmail.com>
2009-11-29 05:58:54 -0500

    Adjusted algorithm to work in the presence of md5 collisions

--- md5match.py
+++ md5match.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import itertools
 import string
 import sys
 
@@ -26,6 +27,12 @@ def isMD5Hash( token ):
                     return False
     return True
 
+# given a list of lines
+# each of the form:
+# <md5hash> <filename>
+# (delimited by whitespace)
+# returns a dictionary mapping
+# MD5 hashes to lists of filenames
 def linesToDict( lines ):
     hashDict = {}
 
@@ -36,19 +43,20 @@ def linesToDict( lines ):
             hash = tokens[ 0 ]
             if( isMD5Hash( hash ) ):
                 # get the rest of the line, starting from the second token
-                value = line[ line.find( tokens[ 1 ] ) : ]
-                hashDict[ hash ] = value
+                filename = line[ line.find( tokens[ 1 ] ) : ]
+                if( hash not in hashDict ):
+                    hashDict[ hash ] = []
+                hashDict[ hash ].append( filename )
 
     return hashDict
 
-def makeEntry( hash, leftFile, rightFile ):
-    return { 'hash':hash, 'left':leftFile, 'right':rightFile }
+def makeEntry( hash, leftFiles, rightFiles ):
+    return { 'hash':hash, 'left':leftFiles, 'right':rightFiles }
 
 leftFilename = sys.argv[ 1 ]
 rightFilename = sys.argv[ 2 ]
 
-# read files
-
+# read files into memory
 leftFile = open( leftFilename )
 leftLines = leftFile.readlines()
 leftFile.close()
@@ -57,10 +65,19 @@ rightFile = open( rightFilename )
 rightLines = rightFile.readlines()
 rightFile.close()
 
-# parse left into a dictionary
+# parse both files into separate dictionaries
 leftDict = linesToDict( leftLines )
 rightDict = linesToDict( rightLines )
 
+# for each hash, sort the list
+for h in leftDict:
+	leftDict[ h ].sort()
+
+for h in rightDict:
+	rightDict[ h ].sort()
+
+# create 3 lists:
+# matches, in-left-not-in-right, and in-right-not-inleft
 matches = []
 inLeftNotInRight = []
 inRightNotInLeft = []
@@ -69,17 +86,27 @@ for hash in leftDict:
     if hash in rightDict:
         matches.append( makeEntry( hash, leftDict[ hash ], rightDict[ hash ] ) )
     else:    
-        inLeftNotInRight.append( makeEntry( hash, leftDict[ hash ], 'NOT_FOUND' ) )
+        inLeftNotInRight.append( makeEntry( hash, leftDict[ hash ], [] ) )
 
+# this operation is asymmetric since
+# if it's in both, it would be picked up by the match step above
 for hash in rightDict:
     if not( hash in leftDict ):
-        inRightNotInLeft.append( makeEntry( hash, 'NOT_FOUND', rightDict[ hash ] ) )
+        inRightNotInLeft.append( makeEntry( hash, [], rightDict[ hash ] ) )
 
+# compare two entries of matching hashes for sorting
 def compareEntries( e0, e1 ):
-    if( e0[ 'left' ] == 'NOT_FOUND' ):
-        return cmp( e0[ 'right' ].lower(), e1[ 'right' ].lower() )
+    
+    # if e0 has nothing on the left
+    # then compare the right entries
+    if( e0[ 'left' ] == [] ):
+        e0RightStr = string.join( e0[ 'right' ] ).lower()
+        e1RightStr = string.join( e1[ 'right' ] ).lower()
+        return cmp( e0RightStr, e1RightStr )
     else:
-        return cmp( e0[ 'left' ].lower(), e1[ 'left' ].lower() )
+        e0LeftStr = string.join( e0[ 'left' ] ).lower()
+        e1LeftStr = string.join( e1[ 'left' ] ).lower()
+        return cmp( e0LeftStr, e1LeftStr )
 
 matches.sort( compareEntries )
 inLeftNotInRight.sort( compareEntries )
@@ -92,14 +119,50 @@ def formatEntries( entries ):
         return '', 0
 
     # find the longest left and right filenames
-    longestLeft = max( [ len( e[ 'left' ] ) for e in entries ] )
-    longestRight = max( [ len( e[ 'right' ] ) for e in entries ] )
+    
+    # list of lists:
+    # [ ['ab', 'cde', 'f'], ['ghij', 'kl'] ]
+    leftFilenames = [ e[ 'left' ] for e in entries ]
+    rightFilenames = [ e[ 'right' ] for e in entries ]
+    
+    # unwrap the list of lists into a single list
+    leftFilenames = list( itertools.chain( *leftFilenames ) )
+    rightFilenames = list( itertools.chain( *rightFilenames ) )
+    
+    longestLeft = max( [ len( f ) for f in leftFilenames ] )
+    longestRight = max( [ len( f ) for f in rightFilenames ] )
 
     formatted = ''
     lineLength = -1
     i = 0
     for e in entries:
-        line = '%4d | %s | %s | %s\n' % ( i, e[ 'hash' ], e[ 'left' ].ljust( longestLeft ), e[ 'right' ].ljust( longestRight ) )
+        
+        h = e[ 'hash' ]
+        spaces = ' ' * len( h )
+        leftList = e[ 'left' ]
+        nLeft = len( leftList )
+        rightList = e[ 'right' ]
+        nRight = len( rightList )
+        isFirst = True
+        
+        for k in range( max( nLeft, nRight ) ):
+            
+            leftStr = ' '
+            rightStr = ' '
+            if( k < nLeft ):
+                leftStr = leftList[ k ]
+
+            if( k < nRight ):
+                rightStr = rightList[ k ]
+            
+            if( isFirst ):
+                if( k < nLeft ):
+                    leftStr
+                line = '%4d | %s | %s | %s\n' % ( i, h, leftStr.ljust( longestLeft ), rightStr.ljust( longestRight ) )
+                isFirst = False
+            else:
+                line = '%4d | %s | %s | %s\n' % ( i, spaces, leftStr.ljust( longestLeft ), rightStr.ljust( longestRight ) )
+        
         if( lineLength < 0 ):
             lineLength = len( line ) - 1
 
@@ -137,3 +200,7 @@ print( '=' * rnlLength )
 print( title )
 print( '=' * rnlLength )
 print( formattedRNL )
+
+# TODO: print summary
+# TODO: display duplicates
+# print( '%d matches, %d only in left, %d only in right' )
