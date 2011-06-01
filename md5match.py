14a8c4659f42d193c8c9c869729da8f0bafd9ad4
Jiawen Chen <jiawen@csail.mit.edu>
2011-06-01 16:50:48 -0400

    Made sorting more reliable by switching to Python 3.2

--- md5match.py
+++ md5match.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 
+import argparse
+import collections
 import itertools
 import string
 import sys
 
-if( len( sys.argv ) ) < 3:
-    print( 'Usage: %s left right\nwhere left and right are md5 filenames' % sys.argv[ 0 ] )
-    sys.exit( 1 )
-
+def readFileIntoLines( filename ):
+    f = open( filename )
+    lines = f.readlines()
+    f.close()
+    return lines
 
 def isMD5Hash( token ):
     token = token.lower()
@@ -50,68 +53,41 @@ def linesToDict( lines ):
 
     return hashDict
 
-def makeEntry( hash, leftFiles, rightFiles ):
-    return { 'hash':hash, 'left':leftFiles, 'right':rightFiles }
+Entry = collections.namedtuple( 'Entry', ['hash', 'left', 'right' ] )
 
-leftFilename = sys.argv[ 1 ]
-rightFilename = sys.argv[ 2 ]
+def entryKey( entry ):
 
-# read files into memory
-leftFile = open( leftFilename )
-leftLines = leftFile.readlines()
-leftFile.close()
-
-rightFile = open( rightFilename )
-rightLines = rightFile.readlines()
-rightFile.close()
-
-# parse both files into separate dictionaries
-leftDict = linesToDict( leftLines )
-rightDict = linesToDict( rightLines )
-
-# for each hash, sort the list
-for h in leftDict:
-	leftDict[ h ].sort()
-
-for h in rightDict:
-	rightDict[ h ].sort()
-
-# create 3 lists:
-# matches, in-left-not-in-right, and in-right-not-inleft
-matches = []
-inLeftNotInRight = []
-inRightNotInLeft = []
-
-for hash in leftDict:
-    if hash in rightDict:
-        matches.append( makeEntry( hash, leftDict[ hash ], rightDict[ hash ] ) )
-    else:    
-        inLeftNotInRight.append( makeEntry( hash, leftDict[ hash ], [] ) )
-
-# this operation is asymmetric since
-# if it's in both, it would be picked up by the match step above
-for hash in rightDict:
-    if not( hash in leftDict ):
-        inRightNotInLeft.append( makeEntry( hash, [], rightDict[ hash ] ) )
-
-# compare two entries of matching hashes for sorting
-def compareEntries( e0, e1 ):
-    
-    # if e0 has nothing on the left
-    # then compare the right entries
-    if( e0[ 'left' ] == [] ):
-        e0RightStr = string.join( e0[ 'right' ] ).lower()
-        e1RightStr = string.join( e1[ 'right' ] ).lower()
-        return cmp( e0RightStr, e1RightStr )
+    if( entry.left == [] ):
+        return entry.right[0]
+    elif( entry.right == [] ):
+        return entry.left[0]
     else:
-        e0LeftStr = string.join( e0[ 'left' ] ).lower()
-        e1LeftStr = string.join( e1[ 'left' ] ).lower()
-        return cmp( e0LeftStr, e1LeftStr )
+        return min( entry.left[0], entry.right[0] )
 
-matches.sort( compareEntries )
-inLeftNotInRight.sort( compareEntries )
-inRightNotInLeft.sort( compareEntries )
+def makeMatchSets( leftDict, rightDict ):
+    # create 3 lists:
+    # matches, in-left-not-in-right, and in-right-not-inleft
+    matches = []
+    inLeftNotInRight = []
+    inRightNotInLeft = []
 
+    # walk over all hashes in the left dictionary
+    # if it exists in the right dictionary, make an entry in matches
+    # otherwise, add it to in-left-not-in-right
+    for hash in leftDict:
+        if hash in rightDict:
+            matches.append( Entry( hash, leftDict[ hash ], rightDict[ hash ] ) )
+        else:    
+            inLeftNotInRight.append( Entry( hash, leftDict[ hash ], [] ) )
+
+    # walk over all the hashes in the right dictionary
+    # test if it's not in the left dictionary and add it to the in-right-not-in-left
+    # dictionary
+    for hash in rightDict:
+        if not( hash in leftDict ):
+            inRightNotInLeft.append( Entry( hash, [], rightDict[ hash ] ) )
+
+    return ( matches, inLeftNotInRight, inRightNotInLeft )
 
 def formatEntries( entries ):
 
@@ -122,15 +98,20 @@ def formatEntries( entries ):
     
     # list of lists:
     # [ ['ab', 'cde', 'f'], ['ghij', 'kl'] ]
-    leftFilenames = [ e[ 'left' ] for e in entries ]
-    rightFilenames = [ e[ 'right' ] for e in entries ]
+    leftFilenames = [ e.left for e in entries ]
+    rightFilenames = [ e.right for e in entries ]
     
     # unwrap the list of lists into a single list
     leftFilenames = list( itertools.chain( *leftFilenames ) )
     rightFilenames = list( itertools.chain( *rightFilenames ) )
-    
-    longestLeft = max( [ len( f ) for f in leftFilenames ] )
-    longestRight = max( [ len( f ) for f in rightFilenames ] )
+
+    longestLeft = 0
+    if leftFilenames != []:
+        longestLeft = max( [ len( f ) for f in leftFilenames ] )
+
+    longestRight = 0
+    if rightFilenames != []:
+        longestRight = max( [ len( f ) for f in rightFilenames ] )
 
     formatted = ''
     lineLength = -1
@@ -138,11 +119,11 @@ def formatEntries( entries ):
 
     for e in entries:
         
-        h = e[ 'hash' ]
+        h = e.hash
         spaces = ' ' * len( h )
-        leftList = e[ 'left' ]
+        leftList = e.left
         nLeft = len( leftList )
-        rightList = e[ 'right' ]
+        rightList = e.right
         nRight = len( rightList )
         isFirst = True
         
@@ -171,37 +152,71 @@ def formatEntries( entries ):
 
     return( formatted, lineLength )
 
-( formattedMatches, matchesLength ) = formatEntries( matches )
-( formattedLNR, lnrLength ) = formatEntries( inLeftNotInRight )
-( formattedRNL, rnlLength ) = formatEntries( inRightNotInLeft )
 
-title = 'Matches'
-if( matchesLength < len( title ) ):
-    matchesLength = len( title )
+def main( argv = None ):
 
-print( '=' * matchesLength )
-print( 'Matches'.center( matchesLength ) )
-print( '=' * matchesLength )
-print( formattedMatches )
+    if argv is None:
+        argv = sys.argv
 
-title = 'In %s, not in %s' % ( leftFilename, rightFilename )
-if( lnrLength < len( title ) ):
-    lnrLength = len( title )
+    parser = argparse.ArgumentParser( description = 'MD5 Matcher' )
+    parser.add_argument( 'left' )
+    parser.add_argument( 'right' )
+    args = parser.parse_args()
 
-print( '=' * lnrLength )
-print( title )
-print( '=' * lnrLength )
-print( formattedLNR )
+    # read files into lines
+    leftLines = readFileIntoLines( args.left )
+    rightLines = readFileIntoLines( args.right )
 
-title = 'In %s, not in %s' % ( rightFilename, leftFilename )
-if( rnlLength < len( title ) ):
-    rnlLength = len( title )
+    # parse both files into separate dictionaries
+    leftDict = linesToDict( leftLines )
+    rightDict = linesToDict( rightLines )
 
-print( '=' * rnlLength )
-print( title )
-print( '=' * rnlLength )
-print( formattedRNL )
+    # for each hash, sort the list of files that match the hash
+    for h in leftDict:
+        leftDict[ h ].sort()
+
+    for h in rightDict:
+        rightDict[ h ].sort()
+
+    (matches, lnr, rnl) = makeMatchSets( leftDict, rightDict )
+    matches.sort( key = entryKey )
+    lnr.sort( key = entryKey )
+    rnl.sort( key = entryKey )
+
+    ( formattedMatches, matchesLength ) = formatEntries( matches )
+    ( formattedLNR, lnrLength ) = formatEntries( lnr )
+    ( formattedRNL, rnlLength ) = formatEntries( rnl )
+
+    title = 'Matches'
+    if( matchesLength < len( title ) ):
+        matchesLength = len( title )
+
+    print( '=' * matchesLength )
+    print( 'Matches'.center( matchesLength ) )
+    print( '=' * matchesLength )
+    print( formattedMatches )
+
+    title = 'In %s, not in %s' % ( args.left, args.right )
+    if( lnrLength < len( title ) ):
+        lnrLength = len( title )
+
+    print( '=' * lnrLength )
+    print( title )
+    print( '=' * lnrLength )
+    print( formattedLNR )
+
+    title = 'In %s, not in %s' % ( args.right, args.left )
+    if( rnlLength < len( title ) ):
+        rnlLength = len( title )
+
+    print( '=' * rnlLength )
+    print( title )
+    print( '=' * rnlLength )
+    print( formattedRNL )
 
 # TODO: print summary
 # TODO: display duplicates
 # print( '%d matches, %d only in left, %d only in right' )
+
+if __name__ == "__main__":
+    sys.exit( main() )
